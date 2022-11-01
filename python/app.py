a006d5c4db4fe14a9c627254866936f16e8a20a8
Solarthenomad <93020734+Solarthenomad@users.noreply.github.com>
2022-11-02 02:11:34 +0900

    Add files via upload

---
+++ python/app.py
@@ -0,0 +1,90 @@
+a=1 /* a라는 변수 안에 오른쪽을 집어넣는다.*/
+b=1
+
+print(type(a))
+/*출력한다. a의 type을*/
+/*4.24e10은 4.24 * 10의 10승을 의미한다.*/
+
+print(a/b) /*나눈 결과값이 그대로 나오고*/
+print(a//b)
+/*나눴을 때의 몫이 출력됨*/
+
+/*문자열*/
+
+a="Hello world"
+a="Python's favorite food is Perl "
+print(type(a))
+
+/*줄 바꾸고 싶을 때는 백슬래쉬+n을 쳐야한다. 파이썬은 한줄씩만 인식하기 때문에 줄바꿈을 그냥 하게 되면 인식을 못함*/
+
+b = 'life is too short \n You need python!'
+print(b)
+
+a="python"
+b = "is fun!"
+print(a*100)
+/*a=python을 100번 출력하게 됨. 문자열에 곱하고 더하기가 가능하다.*/
+print(a+b)
+/*python is fun! 이 출력되게 됨*/
+
+
+#인덱싱(문자열 순서대로 출력하자.)
+a="Life is too short, you need python"
+print(a[0])   /*L이 출력됨. a[0]은 맨 첫번째 문자열이다.*/
+
+print(a[-1]) 
+# o가 출력된다. -가 붙으면 뒤에서부터 출력되는 것 
+
+#슬라이싱(최대와 최소값을 설정간격대로 잘라서 원하는 범위만 출력해주자 )
+c="12345678"
+print(c[::2]) #[이상 : 미만 : 간격]
+#위의 c[::2]에서는 이상값과 미만값이 정해지지 않고 간격만 나왔으니까 1357이 출력되게 된다.
+print(c[:8:])
+#8미만의 모든 값들을 출력하자
+#print(c[:-1:])뒤에서 첫번째 값 미만의 모든 것들을 출력하게 되면 1~7까지의 값이 동일하게 출력됨
+
+number = 10
+day= "three"
+a="I ate %s apples. so I was sick for %s days." %(1.2, day)
+
+print(a)
+
+#정렬과 공백 
+a="%10s % hi"
+print(a) #          hi가 출력된다. 앞에 있는 %s 10개만큼이 띄어진채로 출력됨 
+a="%-10sjane % 'hi'"
+print(a)     #hi            jane이 출력된다. 
+
+#비슷하게 임의로 값을 주는 것중에서 format함수가 있는데, 이 format은 변수의 오른쪽 값에 사용한다. 
+
+a="My age is {age}, My name is {name}".format(name="solarthenomad", age=3)
+#다른 방식으로 format 함수를 축약해서 쓰면 f가 된다.
+name="int"
+a=f"나의 이름은 {name}입니다."
+print(a)
+a.lower() #모두 소문자로 바꾸기
+a.upper() #모두 대문자로 바꾸기 
+#양쪽 공백지우기
+a.strip()
+#문자열 바꾸기 
+a.replace("My age", "Your age") #My age를 Your age로 바꿔주는 것
+
+#%d : 정수 %f : 실수 %s : 문자열(다 사용가능) 이 세개만 사용되니까 이 세개만 외워놓자
+
+#소수점 계산할 때 너무 끝까지 많이 나오면 보기가 꺼려짐 그래서 몇번째 자리수까지만 남겨두고 자른다는 게 필요한데 이때 쓸수있는게 '간격'
+
+d= "%0.4f" % 3.42134234
+#%0.4f에서 0은 간격이고 4는 소수점 남기는 자리수이다.
+#3.4213이 출력되게 되는데 이때 저 위의 말을 해석해보면 3.42134234의 0.4f만큼이 d이다
+print(d)
+
+#함수는 어떤 틀정한 일을 하는 명령어들을 묶어놓은 것이다.
+
+#유용한 명령어들 : count,find, join
+a="hobby"
+print(a.count('b')) /*.은 ~의라는 뜻으로 'a의 b개수를 세라라는 의미임'*/
+print(a.find('b'))/**/
+
+a=",".join("abcd")
+print(a)
+
---
+++ python/dictionary.py
@@ -0,0 +1,60 @@
+#dictionary : AI에서 자주 사용됨 
+#다음은 한 사람에 관한 정보를 갖는 json 객체이다. 키-값 쌍의 패턴으로 표현된다. 
+"""{
+    "이름" : "홍길동",
+    "나이" : 25,
+    "성별" : "여",
+    "주소" : "서울특별시 양천구 목동",
+    "가족관계"}"""
+    
+    #key를 통해 value를 얻는다. 
+    #연관 배열 또는 해쉬
+    
+dictionary = {'name' : 'Eric', 'age' : '15'}
+
+print(dictionary['name']) #딕셔너리의 이름이 dictionary인 것의 key name의 value를 출력 해서 Eric이 출력됨 
+
+a= {1 : 'a'}
+a['name'] = "익명" #a 딕셔너리에 key가 name이고 value가 익명인 원소를 추가해줌
+
+print(a)
+#{1: 'a', 'name' : '익명'}이 출력된다. 
+
+
+grade = {'pay' : 10, 'juliet' : 99}
+print(grade['pay'])  #10이 출력되게 된다. key의 value를 출력해달라는 것
+print(grade['juliet']) #99출력
+
+a={1:'파랑구름', 2:'이현준', 3: '민MIN준JUN'}
+#key값만을 뽑아내기 
+print(a.keys()) #.은 ~의라는 뜻으로 a의 keys를 print해준다는 의미 
+print(a.values()) #.은 ~의라는 뜻으로 a의 values를 프린트해준다는 의미 
+print(a.items()) #key와 value 모두 출력하자
+
+for k in a.keys() :
+    print(k) #a의 key값들의 임의의 k원소들을 출력하라 
+    #1,2,3출력
+
+for k in a.values() :
+    print(k) #a의 value값들의 임의의 k 원소들을 출력하라 
+    #파랑구름, 이현준 , 민MIN준JUN 출력 
+
+for k,v in a.items() :
+    print("키는: " + str(k))
+    print("밸류는" : v)
+    
+print(a[4])
+#이렇게 하면 error만 뜨고 
+print(a.get(4))
+#이렇게 하면 none이 뜨게 되는데 이런 get함수의 성질을 이용해서 default 지정까지 가능하다.
+print(a.get(4,'없음'))
+#
+
+print(1 in a) #a안에 1이라는 key가 있냐 없냐 즉 참과 거짓 결과값을 표현해줌
+
+#집합 자료형
+#중복을 허용하지 않으며 순서가 없다. (인덱싱 ㄴㄴ)
+set = set([1,2,3])
+print(set) #1,2,3출력
+
+
